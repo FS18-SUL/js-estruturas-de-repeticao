528477431a5d9bbb90e415fcac5fdd5c6c6353be
gleidsonteixeira <ronier.lim@gmail.com>
2024-10-16 11:55:15 -0300

    estruturas de repetição com FOR

---
+++ index.js
@@ -0,0 +1,11 @@
+let alunos = ['Camila', 'Nayara', 'Jessica'];
+alunos.push('Frank') // array.push() é a forma correta de inserir valores no final do array
+let mesNascimento = [9, 4, 4];
+mesNascimento.push(2);
+let ehEstudante = [false, true, false];
+ehEstudante.push(false);
+
+// estrutura de repetição for(inicio, condição, incremento/decremento)
+for(let i = 0; i < alunos.length; i++){
+    mensagens.innerHTML += `<h1>Seja bem-vindo(a) ${alunos[i]}</h1>`;
+}
